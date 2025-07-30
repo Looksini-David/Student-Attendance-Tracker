4c83917b541b33c89d8ae087874f3b7824c07a0d
Looksini David <looksinidavid06@gmail.com>
2025-07-30 07:15:41 +0530

    initial version v1.0

---
+++ Student Attendance Tracker/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Student_Attendance_Tracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstAttendance = new System.Windows.Forms.ListBox();
+            this.btnAddRecord = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtStudentName = new System.Windows.Forms.TextBox();
+            this.cmbStatus = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // lstAttendance
+            // 
+            this.lstAttendance.FormattingEnabled = true;
+            this.lstAttendance.ItemHeight = 16;
+            this.lstAttendance.Location = new System.Drawing.Point(97, 274);
+            this.lstAttendance.Name = "lstAttendance";
+            this.lstAttendance.Size = new System.Drawing.Size(642, 148);
+            this.lstAttendance.TabIndex = 0;
+            // 
+            // btnAddRecord
+            // 
+            this.btnAddRecord.Location = new System.Drawing.Point(520, 216);
+            this.btnAddRecord.Name = "btnAddRecord";
+            this.btnAddRecord.Size = new System.Drawing.Size(203, 46);
+            this.btnAddRecord.TabIndex = 1;
+            this.btnAddRecord.Text = "Add Record";
+            this.btnAddRecord.UseVisualStyleBackColor = true;
+            this.btnAddRecord.Click += new System.EventHandler(this.BtnAddRecord_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(128, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Student Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(128, 156);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Attendance Status";
+            // 
+            // txtStudentName
+            // 
+            this.txtStudentName.Location = new System.Drawing.Point(294, 106);
+            this.txtStudentName.Name = "txtStudentName";
+            this.txtStudentName.Size = new System.Drawing.Size(233, 22);
+            this.txtStudentName.TabIndex = 4;
+            // 
+            // cmbStatus
+            // 
+            this.cmbStatus.AutoCompleteCustomSource.AddRange(new string[] {
+            "Present",
+            "Absent"});
+            this.cmbStatus.FormattingEnabled = true;
+            this.cmbStatus.Items.AddRange(new object[] {
+            "Present",
+            "Absent"});
+            this.cmbStatus.Location = new System.Drawing.Point(296, 149);
+            this.cmbStatus.Name = "cmbStatus";
+            this.cmbStatus.Size = new System.Drawing.Size(230, 24);
+            this.cmbStatus.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmbStatus);
+            this.Controls.Add(this.txtStudentName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAddRecord);
+            this.Controls.Add(this.lstAttendance);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstAttendance;
+        private System.Windows.Forms.Button btnAddRecord;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtStudentName;
+        private System.Windows.Forms.ComboBox cmbStatus;
+    }
+}
+
---
+++ Student Attendance Tracker/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Student_Attendance_Tracker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnAddRecord_Click(object sender, EventArgs e)
+        {
+            string name = txtStudentName.Text;
+            string status = cmbStatus.Text;
+            lstAttendance.Items.Add(name + " - " + status);
+            txtStudentName.Clear();
+            cmbStatus.SelectedIndex = -1;
+
+        }
+    }
+}
